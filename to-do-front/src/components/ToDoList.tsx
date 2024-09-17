95465b1d19e266a07e4cc6247d840b0b18137d81
demian-ae <sergio.acuayte@encora.com>
2024-09-16 19:48:24 -0600

    better table

--- to-do-front/src/components/ToDoList.tsx
+++ to-do-front/src/components/ToDoList.tsx
@@ -1,4 +1,5 @@
 import {ToDo} from '../types/ToDo';
+import { FaEdit, FaTrash } from 'react-icons/fa'; // Import the icons you need
 
 interface ToDoListArgs {
     todos: ToDo[],
@@ -8,15 +9,33 @@ interface ToDoListArgs {
 
 export const ToDoList = ({todos, removeTodo}: ToDoListArgs) => {
     return (
-        <ul className="list-group mt-2">
-        {todos.map((todo, index) => (
-            <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
-                <span>
-                    {todo.text}, {todo.dueDate}, {todo.priority}, {todo.done && "hecho"}
-                </span>
-                <button className="btn btn-danger btn-sm" onClick={() => removeTodo(index)}>Delete</button>
-            </li>
-        ))}
-        </ul>
+        <table className="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th scope='col'>Mark</th>
+                    <th scope='col'>Name</th>
+                    <th scope='col'>Priority</th>
+                    <th scope='col'>Due Date</th>
+                    <th scope='col'>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    todos.map((todo, index) => (
+                        <tr key={index}>
+                            <td><input type="checkbox" className='form-check-input' /></td>
+                            <td>{todo.text}</td>
+                            <td>{todo.priority}</td>
+                            <td>{todo.dueDate}</td>
+                            <td>
+                                <button className='btn btn-outline-danger m-1'><FaTrash /></button>
+                                <button className='btn btn-outline-warning m-1'><FaEdit /></button>
+                            </td>
+                        </tr>
+                    ))
+                }
+            </tbody>
+
+        </table>
     )
 }
